51899abd8e2b841c6caee747494da34e91a88d7d
Haeun <han.kim0904@gmail.com>
2024-02-21 11:15:25 +0900

    feat: Reset eslint and style library and Make Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import React from 'react';
 import type { AppProps } from 'next/app';
+import '@/styles/base.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
